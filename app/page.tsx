4ef8aca2e8a1d3112ca10d0fdc231ee827e9dd17
takuya <t.arakawa.it.dev@gmail.com>
2025-11-18 08:04:20 +0900

    UI: TODO入力欄とカードを作成

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <div>
+      {/* 画面センタリング */}
+      <main>
+        {/* 真ん中のカード */}
+        <h1>TODO APP</h1>
+
+        <div>
+          {/* 入力欄＋ボタンを横並び */}
+          <input />
+          <button>追加</button>
+        </div>
+
+      </main>
+    </div>
+  );
+}
